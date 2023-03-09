bcfcafc918e4e5de1d999db1acbdb0013d53b96c
Runqi Tian <runqi@amazon.com>
2023-03-09 22:25:48 +0000

    Add ALTER TABLE test cases

--- src/test/modules/test_ddl_deparse_regress/t/001_compare_dumped_results.pl
+++ src/test/modules/test_ddl_deparse_regress/t/001_compare_dumped_results.pl
@@ -11,35 +11,55 @@ sub execute_test_case {
     my $pub_node = $_[1];
     my $sub_node = $_[2];
     my $dbname = $_[3];
+    my $user = $_[4];
     my $outputdir = $PostgreSQL::Test::Utils::tmp_check;
 
     # set up deparse testing resources
-    create_deparse_testing_resources_on_pub_node($pub_node, $dbname);
+    create_deparse_testing_resources_on_pub_node($pub_node, $dbname, $user);
 
     my $test_file = "./sql/${test_name}.sql";
     my $content = do{local(@ARGV,$/)=$test_file;<>};
+    my $pub_node_error = '';
+    # $pub_node -> psql($dbname, $content,
+    #     stderr => \$pub_node_error,
+    #     on_error_stop => 0,
+    #     extra_params => ["-U ${user}"]);
+    $pub_node -> psql($dbname, $content,
+        stderr => \$pub_node_error,
+        on_error_stop => 0);
+    # check execution of test SQL commands
+    ok($pub_node_error eq '', "execute test SQL commands from ".$test_name) or diag("Failure from "
+        .$test_file.": ".$pub_node_error);
 
-    $pub_node -> psql($dbname, $content);
-
-    # retrieve SQL commands generated from deparsed DDLs on pub node
+    # retrieve reformed SQL commands on pub node, write to file
     my $ddl_sql = '';
     $pub_node -> psql($dbname,q(
         select ddl_deparse_expand_command(ddl) || ';' from deparsed_ddls ORDER BY id ASC),
         stdout => \$ddl_sql);
+    mkdir ${outputdir}."/ddl", 0755;
+    my $ddl_output_file = ${outputdir}."/ddl/${test_name}.sql";
+    open(FH, '>', $ddl_output_file) or die $!;
+    print FH $ddl_sql;
+    close(FH);
 
-    print "\nstart printing re-formed sql\n";
-    print $ddl_sql;
-    print "\nend printing re-formed sql\n";
-    # execute SQL commands on sub node
-    $sub_node -> psql($dbname, $ddl_sql);
+    # execute reformed SQL commands on sub node
+    my $sub_node_error = '';
+    # $sub_node -> psql($dbname, $ddl_sql,
+    #     stderr => \$sub_node_error,
+    #     extra_params => ["-U ${user}"]);
+    $sub_node -> psql($dbname, $ddl_sql,
+        stderr => \$sub_node_error);
+    # check execution of reformed DDL commands
+    ok($sub_node_error eq '', "replay reformed DDL commands from ".$test_name) or diag("Failure from "
+        .$ddl_output_file.": ".$sub_node_error);
 
     # clean up deparse testing resources
     clean_deparse_testing_resources_on_pub_node($pub_node, $dbname);
-
     # dump from pub node and sub node
     mkdir ${outputdir}."/dumps", 0755;
     my $pub_dump = ${outputdir}."/dumps/${test_name}_pub.dump";
     my $sub_dump = ${outputdir}."/dumps/${test_name}_sub.dump";
+    my $dump_diff = ${outputdir}."/dumps/${test_name}_dump.diff";
     system("pg_dumpall "
         . "-s "
         . "-f "
@@ -56,19 +76,22 @@ sub execute_test_case {
         . $sub_node->port)  == 0 or die "Dump sub node failed in ${test_name}";
 
     # compare dumped results
-    is(system("diff "
-    . $pub_dump . " "
-    . $sub_dump), 0, "Dumped results diff in ${test_name}");
+    system("diff ".$pub_dump." ".$sub_dump." > ".$dump_diff);
+    ok(system("diff ".$pub_dump." ".$sub_dump) == 0, "compare dumped results in ${test_name}")
+        or diag("Dumped results are different in ".$test_name
+        .", check ".$dump_diff);
 }
 
 sub init_node {
     my $node_name = $_[0];
     my $node = PostgreSQL::Test::Cluster->new($node_name);
-    $node->init;
     # increase some settings that Cluster->new makes too low by default.
-    $node->adjust_conf('postgresql.conf', 'max_connections', '25');
-    $node->append_conf('postgresql.conf',
-		   'max_prepared_transactions = 10');
+    $node -> init();
+    $node -> start();
+    $node -> append_conf('postgresql.conf', 'max_connections = 25');
+    $node -> append_conf('postgresql.conf', 'client_min_messages = error');
+    $node -> append_conf('postgresql.conf', 'max_prepared_transactions = 10');
+    $node -> restart();
     return $node;
 }
 
@@ -85,14 +108,15 @@ sub init_sub_node {
 sub create_deparse_testing_resources_on_pub_node {
     my $node = $_[0];
     my $dbname = $_[1];
-    $node -> psql($dbname, q(
+    my $user = $_[2];
+    $node -> psql($dbname, "
         begin;
         CREATE EXTENSION test_ddl_deparse_regress;
         create table deparsed_ddls(id SERIAL PRIMARY KEY, tag text, object_identity text, ddl text);
 
         create or replace function deparse_to_json()
             returns event_trigger language plpgsql as
-        $$
+        \$\$
         declare
             r record;
         begin
@@ -101,11 +125,11 @@ sub create_deparse_testing_resources_on_pub_node {
                 insert into deparsed_ddls(tag, object_identity, ddl) values (r.command_tag, r.object_identity, pg_catalog.ddl_deparse_to_json(r.command));
             end loop;
         END;
-        $$;
+        \$\$;
 
         create or replace function deparse_drops_to_json()
             returns event_trigger language plpgsql as
-        $$
+        \$\$
         declare
             r record;
         begin
@@ -114,7 +138,7 @@ sub create_deparse_testing_resources_on_pub_node {
                 insert into deparsed_ddls(tag, object_identity, ddl) values (r.object_type, r.object_identity, public.deparse_drop_ddl(r.object_identity, r.object_type));
             end loop;
         END;
-        $$;
+        \$\$;
 
         create event trigger ddl_deparse_trig
         on ddl_command_end execute procedure deparse_to_json();
@@ -123,7 +147,8 @@ sub create_deparse_testing_resources_on_pub_node {
         on sql_drop execute procedure deparse_drops_to_json();
 
         commit;
-    ));
+    "
+    );
 }
 
 sub clean_deparse_testing_resources_on_pub_node {
@@ -152,13 +177,16 @@ sub trim {
 # Create and start pub sub nodes
 my $pub_node = init_pub_node("test");
 my $sub_node = init_sub_node("test");
-my $dbname = "postgres";
-$pub_node -> start;
-$sub_node -> start;
+my $initial_dbname = "postgres";
+my $user = "ddl_testing_role";
 
 # load test cases from the regression tests
 my @regress_tests = split /\s+/, $ENV{REGRESS};
+my $test_case_count = scalar @regress_tests - 1;
+my $test_count_per_case = 3;
+my $test_count = $test_case_count * $test_count_per_case;
 
+plan tests => $test_count;
 foreach(@regress_tests) {
     my $test_name = trim($_);
     # skip if it's regression test preparation or empty string
@@ -166,16 +194,18 @@ foreach(@regress_tests) {
     {
         next;
     }
-    eval {execute_test_case($test_name, $pub_node, $sub_node, $dbname);};
-    if ($@ ne "")
-    {
-        fail($@);
-    }
+    my $test_dbname = $test_name;
+    # $pub_node -> safe_psql($initial_dbname, "CREATE ROLE ${user} SUPERUSER LOGIN CREATEDB;");
+    # $pub_node -> safe_psql($initial_dbname, "CREATE DATABASE ".$test_dbname, extra_params => ["-U ${user}"]);
+    $pub_node -> safe_psql($initial_dbname, "CREATE DATABASE ".$test_dbname);
+    # $sub_node -> safe_psql($initial_dbname, "CREATE ROLE ${user} SUPERUSER LOGIN CREATEDB;");
+    # $sub_node -> safe_psql($initial_dbname, "CREATE DATABASE ".$test_dbname, extra_params => ["-U ${user}"]);
+    $sub_node -> safe_psql($initial_dbname, "CREATE DATABASE ".$test_dbname);
+    execute_test_case($test_name, $pub_node, $sub_node, $test_dbname, $user);
 }
-close;
+done_testing();
 
 # Close nodes
 $pub_node->stop;
 $sub_node->stop;
 
-done_testing();
